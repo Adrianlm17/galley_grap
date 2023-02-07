9175d9a8f1f359a611dd076d2f4076ab25c29f01
Adrián López <adrianlomu2004@gmail.com>
2023-02-07 11:45:11 +0100

    dev(App | README | pom): implementar datos proporcionados por el profesor.

---
+++ src/main/java/edu/craptocraft/galley_grap/App.java
@@ -0,0 +1,13 @@
+package edu.craptocraft.galley_grap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
